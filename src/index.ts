6504b7e7f0731c6a453999ca5bd15a5372322b4c
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2023-07-21 14:33:52 -0600

    chore: export funtions
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/**
+ * @hyfi06/str-utils
+ * @copyright Copyright (c) 2023 Héctor Olvera Vital
+ * @license MIT
+ */
+
+import { areStringsSimilar } from "./areStringsSimilar";
+import { multiReplace } from "./multiReplaces";
+import { removeAccents } from "./removeAccents";
+import { removeSpaces } from "./removeSpaces";
+import { sanitizeStr } from "./sanitizeStr";
+
+export {
+  areStringsSimilar,
+  multiReplace,
+  removeAccents,
+  removeSpaces,
+  sanitizeStr,
+};
