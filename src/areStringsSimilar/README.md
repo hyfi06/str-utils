4f3519dc0501afe4542d6ed99dfd4b269d3d5796
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2023-07-21 14:28:28 -0600

    feat: add areStringsSimilar function
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

---
+++ src/areStringsSimilar/README.md
@@ -0,0 +1,29 @@
+# Function: areStringsSimilar
+
+The `areStringsSimilar` function compares two strings for similarity. It removes leading, trailing, and duplicate spaces, removes accents, and converts the strings to uppercase before comparing them.
+
+## Parameters
+
+- `str1` (string): The first string to compare.
+- `str2` (string): The second string to compare.
+
+## Returns
+
+- (boolean): Returns `true` if the processed strings are equal, `false` otherwise.
+
+## Example
+
+```javascript
+const str1 = "  HéllO  my  friend!   ";
+const str2 = "hello my friend!";
+const result = areStringsSimilar(str1, str2);
+// result is true
+```
+
+## Copyright
+
+Copyright (c) 2023 Héctor Olvera Vital
+
+## License
+
+MIT
---
+++ src/areStringsSimilar/__test__/areStringsSimilar.test.ts
@@ -0,0 +1,31 @@
+import { areStringsSimilar } from "..";
+
+describe("areStringsSimilar", () => {
+  it("should return true for similar strings", () => {
+    const str1 = "  HéllO  my  friend!   ";
+    const str2 = "hello my friend!";
+    const result = areStringsSimilar(str1, str2);
+    expect(result).toBe(true);
+  });
+
+  it("should return false for dissimilar strings", () => {
+    const str1 = "hello";
+    const str2 = "world";
+    const result = areStringsSimilar(str1, str2);
+    expect(result).toBe(false);
+  });
+
+  it("should handle empty strings", () => {
+    const str1 = "";
+    const str2 = "";
+    const result = areStringsSimilar(str1, str2);
+    expect(result).toBe(true);
+  });
+
+  it("should handle strings with only spaces", () => {
+    const str1 = "     ";
+    const str2 = "";
+    const result = areStringsSimilar(str1, str2);
+    expect(result).toBe(true);
+  });
+});
